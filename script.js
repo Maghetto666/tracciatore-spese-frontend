7fb2004434b487f17e7df79fbd6f495ce531ad31
Maghetto666 <lordrey3@gmail.com>
2024-09-05 15:35:02 +0200

    Iniziato POST

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const inputText = document.querySelector('input');
 const expensesList = document.getElementById('expenses-list');
 const emptyListMsg = document.querySelector('.empty-list-msg');
 const btnDeleteAll = document.getElementById('btnDeleteAll');
+const newDate = document.querySelector('.newDate');
+const newMovement = document.querySelector('.newMovement');
+const newCash = document.querySelector('.newCash');
+const newCategory = document.querySelector('.newCategory');
 
-btnAdd.addEventListener('click', addActivity);
-inputText.addEventListener('keypress', function (event) {
-    if (event.key == "Enter") {
-        addActivity();
-    }
-});
+btnAdd.addEventListener('click', addExpense);
+
 
 fetchExpenses()
 
@@ -20,22 +20,22 @@ async function fetchExpenses() {
 
     expenses = data;
     console.log(expenses);
-    
+
     expensesList.innerHTML = '';
-    
+
     if (expenses.length == 0) {
-    
+
         emptyListMsg.style.display = 'block';
         btnDeleteAll.style.visibility = 'hidden';
     } else {
         emptyListMsg.style.display = 'none';
         btnDeleteAll.style.visibility = 'visible';
-        
+
         for (let i = 0; i < expenses.length; i++) {
             const template = buildTemplateHTML(expenses[i]);
             expensesList.innerHTML += template;
         }
-        
+
         activateChecks();
     }
 }
@@ -57,25 +57,48 @@ function buildTemplateHTML(expense) {
 }
 
 // Funzione che aggiunge una attività alla lista
-function addActivity() {
-    // Recupera testo inserito
-    let text = inputText.value.trim();
-    // Se il testo non è vuoto
-    if (text.length > 0) {
-        // log operazione
-        const now = Date.now();
-        opLogs.push(now + ' - ' + 'aggiunta nuova attività: ' + text);
-        console.log(opLogs[opLogs.length - 1]);
-        // Aggiungiamo all'array
-        activities.push(text);
-        // Aggiungiamo al local storage
-        localStorage.setItem(STORAGE_KEY, JSON.stringify(activities))
-        // Update della schermata
-        showContent();
-        // Cancella test
-        inputText.value = '';
+function addExpense() {
 
-    }
+    let date = newDate.value;
+    let movement = newMovement.value.trim();
+    let cash = newCash.value;
+    let category = newCategory.value;
+
+    let newExpense = {
+        date: date,
+        movement: movement,
+        cash: cash,
+        category: category
+    };
+
+    console.log(newExpense);
+
+    (async () => {
+        const rawResponse = await fetch('http://localhost:8080/expenses', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                date: date,
+                movement: movement,
+                cash: cash,
+                category: category
+             })
+        });
+        const content = await rawResponse.json();
+
+        console.log(content);
+    })();
+            
+        // Cancella test
+        date.value = '';
+        movement.value = '';
+        cash.value = '';
+        category.value = '';
+
+    
 }
 
 // Funzione che abilita le checkbox
@@ -113,46 +136,5 @@ function deleteAll() {
         activities = [];
         localStorage.setItem(STORAGE_KEY, JSON.stringify(activities));
         showContent();
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Struttura appoggio log operazioni
-let opLogs = [];
-// Recupera dai dal local storage
-const STORAGE_KEY = 'todo-key';
-let activities = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-// Funzione che aggiorna il contenuto della lista
-function showContent() {
-
-    // Pulisce la lista
-    expensesList.innerHTML = '';
-    // Se la lista è vuota
-    if (expenses.length == 0) {
-        // Mostra msg lista vuota e nasconde pulsante deleteAll
-        emptyListMsg.style.display = 'block';
-        btnDeleteAll.style.visibility = 'hidden';
-    } else { // Altrimenti
-        // Nasconde msg lista vuota e mostra pulsante deleteAll
-        emptyListMsg.style.display = 'none';
-        btnDeleteAll.style.visibility = 'visible';
-        // Cicliamo gli elementi dell'array
-        for (let i = 0; i < expenses.length; i++) {
-            const template = buildTemplateHTML(expenses[i]);
-            expensesList.innerHTML += template;
-        }
-        // Rende cliccabili le checkbox
-        activateChecks();
     }
 }
